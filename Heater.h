9901d9643c75fa94fe2acb0416eef6e2e8f093fa
Mirco Pergreffi <mircopergreffi@gmail.com>
2022-01-18 00:41:17 +0100

    Update Heater.h

--- Heater.h
+++ Heater.h
@@ -1,19 +1,30 @@
 
-#IFNDEF HEATER_H
-#DEFINE HEATER_H
+#ifndef HEATER_H
+#define HEATER_H
 
-#include "TemperatureSensor.h"
+#include <ArduinoJson.h>
+#include "NTCSensor.h"
 
 class Heater
 {
     public:
-
+        Heater(JsonObject json)
+        {
+            mPin = json["heater_pin"];
+            mTempMax = json["heater_temp_max"];
+            mTempMin = json["heater_temp_min"];
+            mTempCritical = json["heater_temp_critical"];
+            mPowerMax = json["heater_power_max"];
+            mDutyCycleMax = json["heater_duty_cycle_max"];
+            mDutyCycleMin = json["heater_duty_cycle_min"];
+            mSensor = NTCSensor::fromJson(json["heater_sensor"]);
+        }
     private:
         int mPin;
         float mTempMax, mTempMin, mTempCritical;
         float mPowerMax;
         float mDutyCycleMax, mDutyCycleMin;
-        TemperatureSensor * mSensor;
+        NTCSensor mSensor;
 };
 
-#ENDIF /* HEATER_H */
+#endif /* HEATER_H */
