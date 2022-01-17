5d1dc8b6cec2347c2c0741a8e552b0eaeb2584e8
Mirco Pergreffi <mircopergreffi@gmail.com>
2022-01-17 20:27:29 +0100

    Update photon-heater.ino

--- photon-heater.ino
+++ photon-heater.ino
@@ -1,11 +1,66 @@
 
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+
+const char* ssid = "TIM-25786986";
+const char* password = "wR4@SNR6jhnz";
+
+AsyncWebServer server(80);
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  
+  // Serial port for debugging purposes
+  Serial.begin(115200);
 
+  // Initialize SPIFFS
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+  
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/web-client/index.html", "text/html");
+  });
+  
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/web-client/style.css", "text/css");
+  });
+
+  // Route to load script.js file
+  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/web-client/script.js", "text/javascript");
+  });
+  
+  // Route to load config.json file
+  server.on("/config.json", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/config.json", "text/css");
+  });
+
+  // Route to save config.json file
+  server.on("/config.json", HTTP_POST, [](AsyncWebServerRequest *request){
+    
+  });
+
+  // Start server
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
