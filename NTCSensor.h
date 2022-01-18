d70c5bbfb6e7a30847b9ecae0b560c81ab920c3c
Mirco Pergreffi <mircopergreffi@gmail.com>
2022-01-18 01:49:48 +0100

    Update NTCSensor.h

--- NTCSensor.h
+++ NTCSensor.h
@@ -4,33 +4,24 @@
 #define NTC_SENSOR_H
 
 #include <math.h>
+#include <ArduinoJson.h>
 #include "ResistorSensor.h"
 
 class NTCSensor : public ResistorSensor
 {
     public:
-        NTCSensor(int pin, String * name, float pullup, float beta, float R0, float T0)
-        : ResistorSensor(pin, name, pullup)
+        NTCSensor(JsonObject json)
+        : ResistorSensor(json)
         {
-            mBetaInverse = 1.0/beta;
-            mR0 = R0;
-            mT0Inverse = 1.0/T0;
+            mBetaInverse = 1.0/json["beta"].as<float>();
+            mR0 = json["R0"];
+            mT0Inverse = 1.0/json["T0"].as<float>();
         }
         float readValue()
         {
             float R = ResistorSensor::readValue();
             // Steinhart–Hart equation:
             return 1.0/(mT0Inverse + mBetaInverse*log(R/mR0));
-        }
-        static NTCSensor fromJson(JsonObject json)
-        {
-            return NTCSensor(
-                json["pin"],
-                json["name"],
-                json["pullup"],
-                json["beta"],
-                json["R0"],
-                json["T0"]);
         }
     private:
         float mBetaInverse, mR0, mT0Inverse;
