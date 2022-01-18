8c0afb8ca76d204acd436698fe82d64d1b0a36de
Mirco Pergreffi <mircopergreffi@gmail.com>
2022-01-18 22:43:32 +0100

    Update photon-heater.ino

--- photon-heater.ino
+++ photon-heater.ino
@@ -22,18 +22,18 @@ bool readConfigFile()
 	}
 	
 	size_t size = file.size();
-  Serial.print("Size cred file: ");
-  Serial.println(size);
+	Serial.print("Size cred file: ");
+	Serial.println(size);
 	if (size > 2048) {
 		Serial.println("Config file size is too large");
 		return false;
 	}
 	
-  auto error = deserializeJson(doc, file);
+	auto error = deserializeJson(doc, file);
 	if (error)
 	{
 		Serial.println("deserializeJson() failed with code ");
-    Serial.println(error.c_str());
+		Serial.println(error.c_str());
 		return false;
 	}
 
@@ -42,7 +42,7 @@ bool readConfigFile()
 }
 
 void setup() {
-	
+
 	// Serial port for debugging purposes
 	Serial.begin(115200);
 
@@ -54,63 +54,37 @@ void setup() {
 
 	// Read Config
 	if (!readConfigFile())
-  {
-    return;
-  }
+	{
+		return;
+	}
 
-  heater(doc);
-  sensorHeater(doc["heater
+	heater(doc);
+	sensorHeater(doc["heater_sensor"]);
+	sensorAir(doc[""]);
 	
 	// Connect to Wi-Fi
-  const char * ssid = doc["wifi_ssid"];
-  const char * pass = doc["wifi_password"];
+	const char * ssid = doc["wifi_ssid"];
+	const char * pass = doc["wifi_password"];
 	WiFi.begin(ssid, pass);
 	while (WiFi.status() != WL_CONNECTED) {
 		delay(1000);
 		Serial.println("Connecting to WiFi..");
 	}
-
+	
 	// Print ESP32 Local IP Address
 	Serial.println(WiFi.localIP());
+	
+	server.serveStatic("/", SPIFFS, "/");
 
-  server.serveStatic("/", SPIFFS, "/");
-  
 	// Route for root / web page
 	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-		request->send(SPIFFS, "/web-client/index.html");
+		request->send(SPIFFS, "/index.html");
 	});
 	
-//	// Route to load style.css file
-//	server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-//		request->send(SPIFFS, "/web-client/style.css", "text/css");
-//	});
-//
-//  // Route to load script.js file
-//  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request){
-//    request->send(SPIFFS, "/web-client/script.js", "text/javascript");
-//  });
-//  // Route to load script.js file
-//  server.on("/ajax.js", HTTP_GET, [](AsyncWebServerRequest *request){
-//    request->send(SPIFFS, "/web-client/ajax.js", "text/javascript");
-//  });
-// // Route to load chart.min.js file
-//  server.on("/chart.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
-//    request->send(SPIFFS, "/web-client/chart.min.js", "text/javascript");
-//  });
-// // Route to load script.js file
-//  server.on("/chartjs-plugin-annotation.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
-//    request->send(SPIFFS, "/web-client/chartjs-plugin-annotation.min.js", "text/javascript");
-//  });
-//	
-//	// Route to load config.json file
-//	server.on("/config.json", HTTP_GET, [](AsyncWebServerRequest *request){
-//		request->send(SPIFFS, "/config.json", "text/css");
-//	});
-
 	// Start server
 	server.begin();
 }
 
 void loop() {
-
+	
 }
