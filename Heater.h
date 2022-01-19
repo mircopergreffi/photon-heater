f26b0db7e977ea2a71a7d244f80c9f23c5d20deb
Mirco Pergreffi <mircopergreffi@gmail.com>
2022-01-19 16:35:42 +0100

    Update Heater.h

--- Heater.h
+++ Heater.h
@@ -2,36 +2,56 @@
 #ifndef HEATER_H
 #define HEATER_H
 
+#define PWM_Heater_Ch   0
+#define PWM_Heater_Freq 10
+#define PWM_Heater_Rs   8
+#define PWM_Heater_Max  255
+#define PWM_Heater_Duty_Cycle_Max 0.95
+#define PWM_Heater_Duty_Cycle_Min 0.05
+
+#include "Arduino.h"
 #include <ArduinoJson.h>
 
 class Heater
 {
 	public:
+		Heater()
+		{
+
+		}
 		Heater(JsonObject json)
+		{
+			loadFromJson(json);
+		}
+		void loadFromJson(JsonObject json)
 		{
 			mPin = json["heater_pin"];
 			mTempMax = json["heater_temp_max"];
 			mTempMin = json["heater_temp_min"];
 			mTempCritical = json["heater_temp_critical"];
 			mPowerMax = json["heater_power_max"];
-			mDutyCycleMax = json["heater_duty_cycle_max"];
-			mDutyCycleMin = json["heater_duty_cycle_min"];
+			setup();
 		}
 		void setPower(float power)
 		{
 			if (power >= mPowerMax)
 				power = mPowerMax;
-			if (power < mDutyCycleMin)
+			if (power < PWM_Heater_Duty_Cycle_Min)
 				power = 0;
-			else if (power > mDutyCycleMax)
+			else if (power > PWM_Heater_Duty_Cycle_Max)
 				power = 1;
-			analogWrite(mPin, power*255);
+			ledcWrite(PWM_Heater_Ch, power*PWM_Heater_Max);
 		}
 	private:
+		void setup()
+		{
+			ledcAttachPin(mPin, PWM_Heater_Ch);
+			ledcSetup(PWM_Heater_Ch, PWM_Heater_Freq, PWM_Heater_Rs);
+			ledcWrite(PWM_Heater_Ch, 0);
+		}
 		int mPin;
 		float mTempMax, mTempMin, mTempCritical;
 		float mPowerMax;
-		float mDutyCycleMax, mDutyCycleMin;
 };
 
 #endif /* HEATER_H */
