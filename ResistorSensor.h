5ad8472332e1ebd2a0b16720585081813b5039d4
Mirco Pergreffi <mircopergreffi@gmail.com>
2022-01-19 16:35:50 +0100

    Update ResistorSensor.h

--- ResistorSensor.h
+++ ResistorSensor.h
@@ -3,23 +3,34 @@
 #ifndef RESISTOR_SENSOR_H
 #define RESISTOR_SENSOR_H
 
+#include <Arduino.h>
 #include "AnalogSensor.h"
 
 class ResistorSensor : public AnalogSensor
 {
-    public:
-        ResistorSensor(JsonObject json)
-        : AnalogSensor(json)
-        {
-            mPullUp = json["pullup"];
-        }
-        float readValue()
-        {
-            float v = AnalogSensor::readValue();
-            return (v/(1.0-v)) * mPullUp;
-        }
-    private:
-        float mPullUp;
+	public:
+		ResistorSensor()
+		: AnalogSensor()
+		{
+
+		}
+		ResistorSensor(JsonObject json)
+		: AnalogSensor(json)
+		{
+			loadFromJson(json);
+			AnalogSensor::loadFromJson(json);
+		}
+		void loadFromJson(JsonObject json)
+		{
+			mPullUp = json["pullup"];
+		}
+		float readValue()
+		{
+			float v = AnalogSensor::readValue();
+			return (v/(1.0-v)) * mPullUp;
+		}
+	private:
+		float mPullUp;
 };
 
 #endif /* RESISTOR_SENSOR_H */
