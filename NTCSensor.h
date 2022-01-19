786301377252d2a7450606b616786a269a81472f
Mirco Pergreffi <mircopergreffi@gmail.com>
2022-01-19 16:35:44 +0100

    Update NTCSensor.h

--- NTCSensor.h
+++ NTCSensor.h
@@ -9,22 +9,32 @@
 
 class NTCSensor : public ResistorSensor
 {
-    public:
-        NTCSensor(JsonObject json)
-        : ResistorSensor(json)
-        {
-            mBetaInverse = 1.0/json["beta"].as<float>();
-            mR0 = json["R0"];
-            mT0Inverse = 1.0/json["T0"].as<float>();
-        }
-        float readValue()
-        {
-            float R = ResistorSensor::readValue();
-            // Steinhart–Hart equation:
-            return 1.0/(mT0Inverse + mBetaInverse*log(R/mR0));
-        }
-    private:
-        float mBetaInverse, mR0, mT0Inverse;
+	public:
+		NTCSensor()
+		: ResistorSensor()
+		{
+			
+		}
+		NTCSensor(JsonObject json)
+		: ResistorSensor(json)
+		{
+			loadFromJson(json);
+		}
+		void loadFromJson(JsonObject json)
+		{
+			mBetaInverse = 1.0/json["beta"].as<float>();
+			mR0 = json["R0"];
+			mT0Inverse = 1.0/(json["T0"].as<float>()+273.15);
+			ResistorSensor::loadFromJson(json);
+		}
+		float readValue()
+		{
+			float R = ResistorSensor::readValue();
+			// Steinhart–Hart equation:
+			return 1.0/(mT0Inverse + mBetaInverse*log(R/mR0))-273.15;
+		}
+	private:
+		float mBetaInverse, mR0, mT0Inverse;
 };
 
 #endif /* NTC_SENSOR_H */
