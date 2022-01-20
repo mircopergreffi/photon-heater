3e205f8cc3c1d267c79fb05648f7db009ddab42e
Mirco Pergreffi <mircopergreffi@gmail.com>
2022-01-21 00:06:16 +0100

    Update AnalogSensor.h

--- AnalogSensor.h
+++ AnalogSensor.h
@@ -7,38 +7,46 @@
 #include <String.h>
 #include <ArduinoJson.h>
 
+#include <driver/adc.h>
+
 class AnalogSensor
 {
-    public:
-        AnalogSensor()
-        {
+	public:
+		AnalogSensor()
+		{
 
-        }
-        AnalogSensor(JsonObject json)
-        {
-            loadFromJson(json);
-        }
-        void loadFromJson(JsonObject json)
-        {
-            mPin = json["pin"].as<int>();
-            mName = json["name"].as<String>();
-        }
-        virtual float readValue()
-        {
-            return ((float)analogRead(mPin)) / 4095.0;
-        }
-        int getPin()
-        {
-            return mPin;
-        }
-        String getName()
-        {
-            return mName;
-        }
+		}
+		AnalogSensor(JsonObject json)
+		{
+			loadFromJson(json);
+		}
+		void loadFromJson(JsonObject json)
+		{
+			mPin = json["pin"].as<int>();
+			mName = json["name"].as<String>();
+			// adc2_config_width(ADC_WIDTH_12Bit);
+			// adc2_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_11)
+			Serial.print("Sensor ");
+			Serial.print(mName);
+			Serial.print(" on pin ");
+			Serial.println(mPin);
+		}
+		virtual float readValue()
+		{
+			return ((float)analogRead(mPin)) / 4095.0;
+		}
+		int getPin()
+		{
+			return mPin;
+		}
+		String getName()
+		{
+			return mName;
+		}
 
-    private:
-        int mPin;
-        String mName;
+	private:
+		int mPin;
+		String mName;
 };
 
 #endif /* ANALOG_SENSOR_H */
