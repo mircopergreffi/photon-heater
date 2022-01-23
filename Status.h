44d76100e6ef40146414a391ebb4b2177e6a8456
Mirco Pergreffi <mircopergreffi@gmail.com>
2022-01-23 01:56:54 +0100

    Create Status.h

---
+++ Status.h
@@ -0,0 +1,24 @@
+
+#ifndef STATUS_H
+#define STATUS_H
+
+
+enum fan_mode_t
+{
+    FAN_AUTO = 0,
+    FAN_MANUAL = 1
+};
+
+class Status
+{
+    public:
+        volatile float powerHeater;
+        volatile float temperatureSetpoint;
+        volatile float temperatureHeater;
+        volatile float temperatureAir; 
+        // volatile double temperatureResin;
+        volatile fan_mode_t fanMode;
+        volatile float fanManualSpeed;
+}
+
+#endif /* STATUS_H */
